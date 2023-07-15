ee85417cbcb4ee6f2175d974eb7b4cc74a538c32
thong_dt <thong_dt@care-connect.vn>
2023-07-15 11:19:04 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SlideComponent } from './slide/slide.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SlideComponent,
+    IntroductionComponent,
+    ProductsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
